a79d711dafe98593949083d9ddd635e8b402e0fd
Smoopfrog <jeffstinson@hotmail.com>
2023-04-14 01:16:51 -0700

    add reset function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const calculateTotal = (total, tip, people) => {
 const calculate = (event) => {
   event.preventDefault();
 
-  console.log('hello')
+  console.log("hello");
 };
 
 const tipHandler = (event) => {
@@ -22,4 +22,20 @@ const tipHandler = (event) => {
   });
 
   event.target.classList.toggle("active");
+};
+
+const reset = (event) => {
+  event.preventDefault();
+
+  const selectedTip = document.getElementsByClassName("active");
+
+  Array.from(selectedTip).forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  document.getElementById("bill").value = null;
+  document.getElementById("tip").value = null;
+  document.getElementById("people").value = null;
+  document.getElementById("split-tip").innerHTML = "$0.00";
+  document.getElementById("split-total").innerHTML = "$0.00";
 };
